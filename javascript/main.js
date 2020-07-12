51f1d879db1ad0f120f6db234535338152b8a5f7
manjumallesh678 <manjuhinkal00@gmail.com>
2020-07-12 12:23:23 +0530

    Giphy engine

---
+++ javascript/main.js
@@ -0,0 +1,35 @@
+let pre = false;
+document.querySelector(".js-go").addEventListener("click",function(event){
+     let input1 = document.querySelector("input").value;
+        rerender(input1);
+});
+
+document.querySelector(".js-userinput").addEventListener("keyup",function(event){
+   if(event.which === 13){
+        let input1 = document.querySelector("input").value;
+        rerender(input1);
+   }
+});
+function rerender(input1){
+    if(pre){
+        document.querySelector(".js-container").innerHTML = "";
+    }
+    let url = `http://api.giphy.com/v1/gifs/search?q=${input1}&api_key=dc6zaTOxFJmzC`;
+    let giphy = new XMLHttpRequest();
+    giphy.open("GET",url);
+    giphy.send();
+    giphy.addEventListener("load",function(e){
+        let data = JSON.parse(e.target.response);
+        let imgurl = data.data;
+        pushToDom(imgurl);
+    });
+}
+function pushToDom(imgurl){
+    for(let i = 0; i < imgurl.length; i++){
+        let img = document.createElement("img");
+        img.className = "container-image";
+        img.src = imgurl[i].images.fixed_height.url;
+        document.querySelector(".js-container").appendChild(img);
+    }
+    pre = true;
+}
